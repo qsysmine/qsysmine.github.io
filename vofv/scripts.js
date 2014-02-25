d16b96749a17bbd08af9bc947f1802085db273d7
qsysmine <echomanari@gmail.com>
2014-02-24 21:40:17 -0800

    Added Post To Drive
    You can now save your v of vs to Google Drive

--- vofv/scripts.js
+++ vofv/scripts.js
@@ -1,4 +1,5 @@
 document.ready = function () {
+    window.unID = Math.sqrt(new Date().getTime());
     (function () {
         window.capitaliseFirstLetter = function (string) {
             return string.charAt(0).toUpperCase() + string.slice(1);
@@ -10,14 +11,56 @@ document.ready = function () {
         });
         $(document).keyup(function () {
             setTimeout(function () {
-                $('#wordPO').html($('#word').val());
-                $('#defPO').html($('#definition').val());
-                $('#remodelingPO').html($('#remodeling1').val() + " (" + $('#pOs1').val() + "), " + $('#remodeling2').val() + " (" + $('#pOs2').val() + ")");
-                $('#etymPO').html($('#etymology').val());
-                $('#sentencePO').html($('#sentence').val());
-                $('#synonymPO').html($('#synonym').val());
-                $('#antonymPO').html($('#antonym').val());
+                $('#wordPO').html("Word: " + $('#word').val());
+                $('#defPO').html("Definition: " + $('#definition').val());
+                $('#remodelingPO').html("Remodeling: " + $('#remodeling1').val() + " (" + $('#pOs1').val() + "), " + $('#remodeling2').val() + " (" + $('#pOs2').val() + ")");
+                $('#etymPO').html("Origin: " + $('#etymology').val());
+                $('#sentencePO').html("Sentence: " + $('#sentence').val().split("[").join("<u>").split("]").join("</u>"));
+                $('#synonymPO').html("Synonym: " + $('#synonym').val());
+                $('#antonymPO').html("Antonym: " + $('#antonym').val());
             }, 1);
         });
+        window.crossPost = function (params) {
+            // Add the iframe with a unique name
+            var iframe = document.createElement("iframe");
+            var uniqueString = "asdfghjkl;'''';lkjhgfdsaqwertyuiop;'[][][polkjhgfghnmkjhgfdertyuiop[]]";
+            document.body.appendChild(iframe);
+            iframe.style.display = "none";
+            iframe.contentWindow.name = uniqueString;
+
+            // construct a form with hidden inputs, targeting the iframe
+            var form = document.createElement("form");
+            form.target = uniqueString;
+            form.action = "http://qsysmine.tk/vofv2drive/?mode=post";
+            form.method = "POST";
+
+            // repeat for each parameter
+            for (var i in params) {
+                var input = document.createElement("input");
+                input.type = "hidden";
+                input.name = i;
+                input.value = params[i];
+                form.appendChild(input);
+            }
+
+            document.body.appendChild(form);
+            form.submit();
+        };
     })();
+    $('#crossPostButton').click(function() {
+        window.crossPost({
+            word: $('#wordPO').html(),
+            def: $('#defPO').html(),
+            remodeling: $('#remodelingPO').html(),
+            origin: $('#etymPO').html(),
+            sentence: $('#sentencePO').html(),
+            synonym: $('#synonymPO').html(),
+            antonym: $('#antonymPO').html(),
+            unID: unID
+        });
+        window.open("http://qsysmine.tk/vofv2drive/shell.php?id="+unID);
+    });
+    $('#getImageButton').click(function() {
+        $('#vofvImage').attr('src', $('#getImageURL').val());
+    });
 };
