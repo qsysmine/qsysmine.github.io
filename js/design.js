104e2ad1001c5451154c768c609b6996ddc8d427
Ari Stassinopoulos <stassinopoulosari@gmail.com>
2020-09-24 21:36:23 -0700

    Change logo image, modify home page styling, add link to engineering portfolio

--- js/design.js
+++ js/design.js
@@ -3,7 +3,9 @@ $(function() {
     $.getJSON(url, function(data) {
         if($('#designContainer').length) {
             var pics = data.data;
-            pics.forEach(function(a, b) {
+            pics.sort((a,b) => {
+              return 0.5 - Math.random();
+            }).forEach(function(a, b) {
                 var downloadURL = a.download_url;
                 var picURL = downloadURL.replace("raw.githubusercontent.com", "cdn.rawgit.com").replace("http:", "https:");
                 $('#designContainer').append("<img class='grid-item' src='" + picURL + "'>");
